e99ac9a5bd473d4a12e2201d3c54b4cdcd5f3ea5
Santiago dos Santos <sdossantos@asap-consulting.net>
2024-07-31 22:06:05 -0300

    Texto y props CityItem

--- src/components/cities/CityItem.tsx
+++ src/components/cities/CityItem.tsx
@@ -1,19 +1,36 @@
 import { Box } from '@mui/material'
 import '../../styles/cities/CityItem.css'
 
-export function CityItem() {
+interface CityItemProps {
+  title: string
+  subtitle: string
+  imageUrl: string
+}
+
+export function CityItem({ title, subtitle, imageUrl }: CityItemProps) {
   return (
     <Box
       className="city-item"
       sx={{
-        background:
-          'url(https://media.cntraveler.com/photos/58ecf3b92ec89232fccf6417/16:9/w_2560%2Cc_limit/GettyImages-141946945.jpg)',
+        background: 'url(' + imageUrl + ') no-repeat center center / cover',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <h1>Senegal</h1>
+      <Box
+        sx={{
+          color: 'white',
+          textAlign: 'left',
+          padding: '5%',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+        }}
+      >
+        <h2 style={{ fontSize: '4em' }}>{title}</h2>
+        <h3>{subtitle}</h3>
+      </Box>
     </Box>
   )
 }
--- src/components/cities/CityList.tsx
+++ src/components/cities/CityList.tsx
@@ -7,12 +7,21 @@ import { CityItem } from './CityItem'
 
 export default function CityList() {
   return (
-    <Swiper navigation={true} modules={[Navigation]}>
+    <Swiper navigation modules={[Navigation]}>
       <SwiperSlide>
-        <CityItem />
+        <CityItem
+          title="Destino Senegal"
+          subtitle="Una Aventura A Vivir"
+          imageUrl="https://media.cntraveler.com/photos/58ecf3b92ec89232fccf6417/16:9/w_2560%2Cc_limit/GettyImages-141946945.jpg
+"
+        />
       </SwiperSlide>
       <SwiperSlide>
-        <CityItem />
+        <CityItem
+          title="Destino Isla Pepito"
+          subtitle="Una aventura en la que te podes caer"
+          imageUrl="https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?cs=srgb&dl=pexels-pixabay-147411.jpg&fm=jpg"
+        />
       </SwiperSlide>
     </Swiper>
   )
