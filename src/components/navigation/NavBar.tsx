ef22738d08d0ae62799b4456967224acca54697d
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-08-02 14:11:42 -0300

    Fonts y navbar

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,31 +1,71 @@
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
 import Toolbar from '@mui/material/Toolbar'
-import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography'
+import { NavItemProps } from '../../interfaces/nav-item-props'
+import Divider from '@mui/material/Divider'
 
-export default function NavBar() {
+export default function NavBar({ navItems }: { navItems: NavItemProps[] }) {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar
         position="static"
-        color="transparent"
         sx={{
-          backdropFilter: 'blur(30px)',
-          padding: 1,
-          borderRadius: 4,
-          color: 'white',
-          background:
-            'linear-gradient(90deg, rgba(255,255,255,0.9) 0%, rgba(255,255,255,1) 35%, rgba(212,236,223,1) 100%)',
+          height: 235,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          flexDirection: 'column',
+          backgroundColor: 'white',
+          color: 'rgb(62,62,62)',
+          boxShadow: 'none',
         }}
       >
-        <Toolbar>
+        <Toolbar
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            flexDirection: 'column',
+            gap: 4,
+          }}
+        >
           <img
-            src="https://i.ibb.co/WDNpKR0/TRIPFINDER-120-x-66-px.png"
-            style={{ transform: 'scale(0.5)' }}
+            src="https://static.wixstatic.com/media/19b022_db51b8dad80040fc8afa14a0c1d1f377~mv2.gif"
+            width={204}
+            height={120}
           />
-          <Button color="inherit">Login</Button>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              gap: 4,
+            }}
+          >
+            {navItems.map((item: NavItemProps) => (
+              <Typography
+                key={item.label}
+                component="a"
+                href={item.href}
+                sx={{
+                  fontFamily: 'Aceh',
+                  fontSize: '1em',
+                  padding: '2px 16px',
+                  color: 'rgb(62,62,62)',
+                  textDecoration: 'none',
+                  transition: 'background-color 0.5s ease',
+                  ':hover': { backgroundColor: 'rgb(187,149,147, 0.8)' },
+                }}
+              >
+                {item.label}
+              </Typography>
+            ))}
+          </Box>
         </Toolbar>
       </AppBar>
+
+      <Divider orientation="horizontal" flexItem />
     </Box>
   )
 }
