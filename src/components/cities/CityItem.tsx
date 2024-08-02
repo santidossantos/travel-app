9831eccf19000fec77bc51b85dde00e2d5f54daa
paguero8 <paguero@asap-consulting.net>
2024-08-02 16:45:10 -0300

    fonts changes

--- src/components/cities/CityItem.tsx
+++ src/components/cities/CityItem.tsx
@@ -1,12 +1,12 @@
 import '../../styles/cities/CityItem.css'
 import { CityItemProps } from '../../interfaces/city-item-props'
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 export function CityItem({ title, subtitle, imageUrl }: CityItemProps) {
   return (
@@ -18,19 +18,33 @@ export function CityItem({ title, subtitle, imageUrl }: CityItemProps) {
           </IconButton>
         }
         title={
-          <Typography variant='h6'
-          sx={{ color: 'primary.main', fontWeight: 'bold' }}>
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: 'Playfair Display',
+              fontSize: '1.2em',
+              color: 'rgb(71, 71, 71)',
+              fontWeight: 'bold',
+            }}
+          >
             {title}
           </Typography>
         }
-        subheader={subtitle}
+        subheader={
+          <Typography
+            variant="h6"
+            sx={{
+              fontFamily: 'Crimson Text',
+              fontSize: '1.3em',
+              fontStyle: 'italic',
+              color: 'rgb(71, 71, 71)',
+            }}
+          >
+            {subtitle}
+          </Typography>
+        }
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image={imageUrl}
-        alt={title}
-      />
+      <CardMedia component="img" height="194" image={imageUrl} alt={title} />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
           This impressive paella is a perfect party dish and a fun meal to cook
@@ -41,4 +55,3 @@ export function CityItem({ title, subtitle, imageUrl }: CityItemProps) {
     </Card>
   )
 }
-
