119b1966d658876e3af9ddf3b94cbb2c70b8848b
paguero8 <paguero@asap-consulting.net>
2024-08-05 11:02:00 -0300

    slider

--- src/components/cities/CityList.tsx
+++ src/components/cities/CityList.tsx
@@ -4,19 +4,26 @@ import '../../styles/cities/CityList.css'
 import { CityItem } from './CityItem'
 import { CityListProps } from '../../interfaces/city-list-props'
 import { Box } from '@mui/material'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import { Navigation } from 'swiper/modules'
 
 export default function CityList({ cities }: CityListProps) {
   return (
     <Box>
-      {cities.map((city, index) => (
-        <CityItem
-          key={index}
-          title={city.title}
-          subtitle={city.subtitle}
-          imageUrl={city.imageUrl}
-          description={city.description}
-        />
-      ))}
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+        {cities.map((city, index) => (
+          <SwiperSlide key={index}>
+            <CityItem
+              title={city.title}
+              subtitle={city.subtitle}
+              imageUrl={city.imageUrl}
+              description={city.description}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </Box>
   )
 }
