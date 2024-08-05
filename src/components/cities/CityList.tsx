4f22115ab9a126224f894fc54d25364d4f1179c0
Santiago dos Santos <sdossantos@asap-consulting.net>
2024-08-05 12:01:45 -0300

    Fix swiper

--- src/components/cities/CityList.tsx
+++ src/components/cities/CityList.tsx
@@ -1,29 +1,31 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
 import 'swiper/css'
 import 'swiper/css/navigation'
+
 import '../../styles/cities/CityList.css'
+
+// import required modules
+import { Navigation } from 'swiper/modules'
+
 import { CityItem } from './CityItem'
 import { CityListProps } from '../../interfaces/city-list-props'
-import { Box } from '@mui/material'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import 'swiper/css/navigation'
-import { Navigation } from 'swiper/modules'
 
 export default function CityList({ cities }: CityListProps) {
   return (
-    <Box>
-      <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-        {cities.map((city, index) => (
-          <SwiperSlide key={index}>
-            <CityItem
-              title={city.title}
-              subtitle={city.subtitle}
-              imageUrl={city.imageUrl}
-              description={city.description}
-            />
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </Box>
+    <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
+      {cities.map((city, index) => (
+        <SwiperSlide key={index}>
+          <CityItem
+            title={city.title}
+            subtitle={city.subtitle}
+            imageUrl={city.imageUrl}
+            description={city.description}
+          />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   )
 }
