c8c9c34dd711ced96e2f02c1f57872b55f2c7aa0
paguero8 <paguero@asap-consulting.net>
2024-08-06 15:13:59 -0300

    update footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,42 +1,104 @@
-import { GitHub, LinkedIn, Twitter } from '@mui/icons-material'
+import { GitHub } from '@mui/icons-material'
 import { Box, Container, Typography, Link, IconButton } from '@mui/material'
 
 export function Footer() {
   return (
-    <Box>
+    <Box
+      sx={{
+        backgroundColor: 'rgb(243,236,235)',
+        color: 'rgb(71, 71, 71)',
+        padding: '25px',
+      }}
+    >
       <Container>
-        <Typography variant="h6" align="center" gutterBottom>
+        <Typography
+          variant="h6"
+          align="center"
+          gutterBottom
+          sx={{
+            fontFamily: 'Playfair Display',
+            fontSize: '1.3em',
+          }}
+        >
+          Integrantes del grupo
+        </Typography>
+        <Typography
+          variant="h6"
+          align="center"
+          gutterBottom
+          sx={{
+            fontFamily: 'Aceh',
+          }}
+        >
           <Link
-            href="https://github.com/santidossantos"
+            href="https://www.linkedin.com/in/santiago-dos-santos/"
             color="inherit"
             underline="hover"
           >
             Santiago Dos Santos
           </Link>
-          ,
+        </Typography>
+        <Typography
+          variant="h6"
+          align="center"
+          gutterBottom
+          sx={{ fontFamily: 'Aceh' }}
+        >
           <Link
-            href="https://github.com/paguero8"
+            href="https://www.linkedin.com/in/agueropaula/"
             color="inherit"
             underline="hover"
           >
             Paula Agüero
           </Link>
         </Typography>
-        <Typography variant="body2" align="center" gutterBottom>
-          Contáctanos: (123) 456-7890
+        <Typography
+          variant="body2"
+          align="center"
+          gutterBottom
+          sx={{ fontFamily: 'Aceh', fontSize: '1.2em' }}
+        >
+          info@tripfinder.com
         </Typography>
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
-          <IconButton href="https://github.com/santidossantos" color="inherit">
-            <GitHub />
-          </IconButton>
-          <IconButton href="https://twitter.com/yourprofile" color="inherit">
-            <Twitter />
-          </IconButton>
-          <IconButton
-            href="https://www.linkedin.com/in/yourprofile"
-            color="inherit"
+        <Typography
+          variant="body2"
+          align="center"
+          gutterBottom
+          sx={{ fontFamily: 'Aceh', fontSize: '1.2em' }}
+        >
+          +54 9 1234567890
+        </Typography>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignContent: 'center',
+          }}
+        >
+          <Typography
+            variant="body2"
+            align="center"
+            gutterBottom
+            sx={{ fontFamily: 'Aceh', fontSize: '1.2em', fontWeight: 'bold' }}
           >
-            <LinkedIn />
+            Repositorio
+          </Typography>
+          <IconButton
+            href="https://github.com/santidossantos/travel-app"
+            color="inherit"
+            sx={{
+              transform: 'scale(1.5)',
+              '&:hover': {
+                backgroundColor: 'transparent',
+                boxShadow: 'none',
+              },
+              '&:focus': {
+                boxShadow: 'none',
+              },
+            }}
+          >
+            <GitHub />
           </IconButton>
         </Box>
       </Container>
