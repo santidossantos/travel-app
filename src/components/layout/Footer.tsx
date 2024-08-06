5a0a9394f14164d191c57e70e90b207622528f0e
paguero8 <paguero@asap-consulting.net>
2024-08-06 12:02:17 -0300

    footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,45 @@
-import { Box } from '@mui/material'
+import { GitHub, LinkedIn, Twitter } from '@mui/icons-material'
+import { Box, Container, Typography, Link, IconButton } from '@mui/material'
 
 export function Footer() {
-  return <Box></Box>
+  return (
+    <Box>
+      <Container>
+        <Typography variant="h6" align="center" gutterBottom>
+          <Link
+            href="https://github.com/santidossantos"
+            color="inherit"
+            underline="hover"
+          >
+            Santiago Dos Santos
+          </Link>
+          ,
+          <Link
+            href="https://github.com/paguero8"
+            color="inherit"
+            underline="hover"
+          >
+            Paula Agüero
+          </Link>
+        </Typography>
+        <Typography variant="body2" align="center" gutterBottom>
+          Contáctanos: (123) 456-7890
+        </Typography>
+        <Box sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}>
+          <IconButton href="https://github.com/santidossantos" color="inherit">
+            <GitHub />
+          </IconButton>
+          <IconButton href="https://twitter.com/yourprofile" color="inherit">
+            <Twitter />
+          </IconButton>
+          <IconButton
+            href="https://www.linkedin.com/in/yourprofile"
+            color="inherit"
+          >
+            <LinkedIn />
+          </IconButton>
+        </Box>
+      </Container>
+    </Box>
+  )
 }
