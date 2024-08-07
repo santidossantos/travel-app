0be9a5f122ea13b0aa182ac39eec81a11f0c2206
Santiago dos Santos <santiiagodossantos@icloud.com>
2024-08-07 15:11:11 -0300

    Contact section, styles

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,4 +1,4 @@
-import { Box, InputLabel, TextField, Typography } from '@mui/material'
+import { Box, Button, TextField, Typography } from '@mui/material'
 
 export function ContactForm() {
   return (
@@ -8,7 +8,8 @@ export function ContactForm() {
         flexDirection: 'column',
         alignItems: 'center',
         gap: '2rem',
-        padding: '3rem',
+        padding: '3rem 0',
+        width: '100%',
       }}
     >
       <img
@@ -23,17 +24,63 @@ export function ContactForm() {
           fontSize: '2.5rem',
           fontWeight: '600',
           marginTop: '2rem',
+          letterSpacing: '0.2rem',
         }}
       >
         CONTACTO
       </Typography>
-      <form noValidate autoComplete="off">
+      <form
+        noValidate
+        autoComplete="off"
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '2rem',
+          width: '80%',
+          maxWidth: '780px',
+        }}
+      >
+        <TextField
+          id="standard-helperText"
+          label="Email"
+          defaultValue="Enter your email"
+          variant="standard"
+        />
         <TextField
           id="standard-helperText"
           label="Name"
           defaultValue="Enter your name"
           variant="standard"
         />
+        <TextField
+          id="standard-helperText"
+          label="Name"
+          defaultValue="Enter your name"
+          variant="standard"
+        />
+        <TextField
+          id="standard-helperText"
+          label="Name"
+          defaultValue="Enter your name"
+          variant="standard"
+          multiline
+          rows={4}
+        />
+
+        <Button
+          variant="contained"
+          sx={{
+            backgroundColor: 'rgb(137 89 86)',
+            padding: '0.6rem 0',
+            borderRadius: '0',
+            fontFamily: 'Raleway, sans-serif',
+            fontSize: '1rem',
+            fontWeight: '400',
+            textTransform: 'none',
+          }}
+        >
+          Enviar
+        </Button>
       </form>
     </Box>
   )
